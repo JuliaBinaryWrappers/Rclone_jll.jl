e6f46b5596ff63287ed33d9e743ebb4bef59061b
jlbuild <juliabuildbot@gmail.com>
2026-04-01 22:57:14 +0000

    Rclone_jll build 1.73.3+0

--- src/Rclone_jll.jl
+++ src/Rclone_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Rclone")
-JLLWrappers.@generate_main_file("Rclone", UUID("d3707c4a-c138-5086-90bb-c65de4e50315"))
+JLLWrappers.@generate_main_file("Rclone", Base.UUID("d3707c4a-c138-5086-90bb-c65de4e50315"))
 end  # module Rclone_jll
